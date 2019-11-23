85db77ac07fa11d0b8db94bebbf15c9f18138a86
Marcin Wątroba <marcin.watroba@deviniti.com>
2019-11-23 20:25:45 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from flask import Flask, request
+import pandas as pd
+from random import shuffle
+from flask_cors import CORS, cross_origin
+import json
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+TWEETS_FILE = "data/data.json"
+
+
+def read_all_tweets():
+    return pd.read_json(TWEETS_FILE)
+
+
+def update_all_tweets(df):
+    df.to_json(TWEETS_FILE)
+    pass
+
+
+def get_df_id_by_tweet_id(df, tweet_id):
+    return df[df['tweet_id'] == tweet_id].index.tolist()[0]
+
+
+@app.route("/get_unlabelled_tweet")
+@cross_origin()
+def get_unlabelled_tweet():
+    tweets = read_all_tweets()
+    arr = [(it[0], it[1]) for it in tweets[tweets.label == 'NONE'].to_numpy()]
+    shuffle(arr)
+    response_body = json.dumps(
+        {
+            'id': arr[0][0],
+            'tweet': arr[0][1]
+        }
+    )
+    return response_body
+
+
+@app.route("/save_label", methods=['PUT'])
+@cross_origin()
+def save_label():
+    request_body = request.get_json()
+    tweet_id = request_body['tweet_id']
+    label = request_body['label']
+    tweets = read_all_tweets()
+    row_id = get_df_id_by_tweet_id(tweets, tweet_id)
+    tweets.xs(row_id)['label'] = label
+    update_all_tweets(tweets)
+    return '{}'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
