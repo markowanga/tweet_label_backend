d6a578dbc56412b0493815f628219590bde8e266
Marcin Wątroba <marcin.watroba@deviniti.com>
2019-12-03 22:57:25 +0100

    Make new api for labelling verification

---
+++ main_user.py
@@ -0,0 +1,99 @@
+from flask import Flask, request, jsonify, send_file, send_from_directory
+import pandas as pd
+from flask_cors import CORS, cross_origin
+import seaborn as sns
+import matplotlib.pyplot as plt
+import mongo_user_methods as mmu
+
+app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app)
+
+
+def group_by_counts(df):
+    values = list(df['label'].to_numpy().T)
+    unique = set(values)
+    dic = {}
+    for it in unique:
+        dic[it] = values.count(it)
+    return dic
+
+
+@app.route("/get_unlabelled_tweet", methods=['GET'])
+@cross_origin()
+def get_unlabelled_tweet():
+    username = request.form['username']
+    tweet = mmu.get_random_not_labelled_tweet_by_username(username)
+    return jsonify({
+        'id': tweet['tweet_id'],
+        'tweet': tweet['tweet_content']
+    })
+
+
+@app.route("/save_label", methods=['PUT'])
+@cross_origin()
+def save_label():
+    request_body = request.get_json()
+    tweet_id = request_body['tweet_id']
+    label = request_body['label']
+    username = request_body['username']
+    note = request_body['note']
+    mmu.save_label_for_user(tweet_id, label, username, note)
+    return '', 204
+
+
+@app.route("/stats", methods=['GET'])
+@cross_origin()
+def get_stats():
+    tweets = mmu.get_df_with_all()
+    all_tweets_count = tweets.shape[0]
+    labelled_tweets_count = tweets[tweets.apply(lambda x: x.label != '', axis=1)].shape[0]
+    return jsonify({
+        'all_tweets_count': all_tweets_count,
+        'labelled_tweets_count': labelled_tweets_count
+    })
+
+
+@app.route("/stats_detailed", methods=['GET'])
+@cross_origin()
+def get_stats_detailed():
+    tweets = mmu.get_df_with_all()
+    return jsonify(group_by_counts(tweets))
+
+
+@app.route("/labelled_tweets", methods=['GET'])
+@cross_origin()
+def get_labelled_tweets():
+    tweets = mmu.get_df_with_all()
+    tweets = tweets[tweets.label != ''].sort_values(by=['update_time'], ascending=False)
+    list_to_return = []
+    for index, row in tweets.iterrows():
+        d = row.to_dict()
+        d['update_time'] = d['update_time'].isoformat()
+        d['note'] = ''
+        d['tweet'] = d['tweet_content']
+        d.pop('_id', None)
+        d.pop('tweet_content', None)
+        list_to_return.append(d)
+    return jsonify({'tweets': list_to_return})
+
+
+@app.route('/stats_heatmap')
+@cross_origin()
+def get_stats_heatmap():
+    flights = mmu.get_df_with_all()
+    flights = pd.pivot_table(flights, values='tweet_id', index=['label'],  columns=['username'], aggfunc=len)
+    fig, ax = plt.subplots(figsize=(12, 3))
+    sns.heatmap(flights, annot=True, fmt=".0f")
+    fig.savefig('output.png')
+    return send_file("output.png", mimetype='image/gif', cache_timeout=0)
+
+
+@app.route('/get_all_results', methods=['GET'])
+def download():
+    mmu.get_df_with_all().to_json('results.json')
+    return send_from_directory(directory='', filename="results.json")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
--- mongo_user_methods.py
+++ mongo_user_methods.py
@@ -7,35 +7,44 @@ mongo_client = pymongo.MongoClient("mongodb://root:password@192.168.0.124:27017/
 labelling_db = mongo_client['tweets_labelling']
 abortion_collection = labelling_db['abortion']
 
+ALL_USERS = ['Daniel', 'Dawid', 'Marcin', 'Maciek']
+
 
 def query_from_tweet_id(tweet_id):
     return {'tweet_id': tweet_id}
+
+
+def query_from_tweet_id_and_username(tweet_id, username):
+    return {'tweet_id': tweet_id, 'username': username}
 
 
 def get_all_tweets():
     return [it for it in abortion_collection.find()]
 
 
+def get_all_tweets_by_user(username):
+    return [it for it in abortion_collection.find() if it['username'] == username]
+
+
 def random_index(n):
     return random.randrange(0, n)
 
 
-def get_random_not_labelled_tweet():
-    not_labelled_tweets = [it for it in get_all_tweets() if it['label'] == '']
+def get_random_not_labelled_tweet_by_username(username):
+    not_labelled_tweets = [it for it in get_all_tweets() if it['label'] == '' and it['username'] == username]
     items_count = len(not_labelled_tweets)
     return not_labelled_tweets[random_index(items_count)] if items_count > 0 else None
 
 
-def save_label(tweet_id, label, username, note):
+def save_label_for_user(tweet_id, label, username, note):
     new_values = {
         "$set": {
             "label": label,
-            'username': username,
             'note': note,
             'update_time': datetime.datetime.now()
         }
     }
-    abortion_collection.update_one(query_from_tweet_id(tweet_id), new_values)
+    abortion_collection.update_one(query_from_tweet_id_and_username(tweet_id, username), new_values)
     return
 
 
@@ -49,14 +58,13 @@ def get_df_with_all():
     return df
 
 
-def insert_all_tweets(tweets):
+def insert_tweets_for_all_user(tweets):
     tweets_to_insert = [
         {
             'tweet_id': it['tweet_id'],
             'tweet_content': it['tweet_content'],
             'hashtags': it['hashtags'],
             'label': it['label'],
-            'username': it['username'],
             'insert_time': datetime.datetime.now(),
             'update_time': it['update_time']
         }
