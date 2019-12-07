84576e26c9b23b3c6c7986ffbdf718be61d5471c
Marcin Wątroba <marcin.watroba@deviniti.com>
2019-12-07 18:15:02 +0100

    Change tweet_labels_file

--- stats_generator.py
+++ stats_generator.py
@@ -8,10 +8,12 @@ def generate_text_with_report_by_tweets(tweets_label_df: pd.DataFrame):
     def add_line(line):
         final_string_list.append(line)
 
-    tweet_id_list = list(set(list(tweets_label_df.sort_values('insert_time', ascending=True).tail(400)['tweet_id'].to_numpy())))
+    filtered = tweets_label_df.sort_values('insert_time').tail(400)
+
+    tweet_id_list = list(set(list(filtered.sort_values('insert_time', ascending=True).tail(400)['tweet_id'].to_numpy())))
     for tweet_id_index in range(len(tweet_id_list)):
         tweet_id = tweet_id_list[tweet_id_index]
-        selected_tweets = tweets_label_df[tweets_label_df['tweet_id'] == tweet_id]
+        selected_tweets = filtered[filtered['tweet_id'] == tweet_id]
         add_line('--------------------')
         add_line('')
         add_line('#' + str(tweet_id_index + 1))
